f012d1b9a8ccc353d81be47a9ed6a94b22ffeffc
beamm536 <beamm023@gmail.com>
2024-12-28 21:43:37 +0100

    comenzamos con la incorporacion de mvvm
    > creado un apartado de Login para la practica :)

--- app/src/main/java/com/appclass/myapplication/NavigationWrapper.kt
+++ app/src/main/java/com/appclass/myapplication/NavigationWrapper.kt
@@ -3,6 +3,7 @@ package com.appclass.myapplication
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,16 +12,18 @@ import com.appclass.myapplication.screens.PantallaFormulario1
 import com.appclass.myapplication.screens.ScreenForm1
 import com.appclass.myapplication.screens.ScreenForm2
 import androidx.navigation.compose.rememberNavController
+import com.appclass.myapplication.ui.login.ui.LoginScreen
 
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NavigationWrapper(navHostController: NavHostController){
-    NavHost(navController = navHostController, startDestination = "screenForm1") {
+    NavHost(navController = navHostController, startDestination = "loginScreen") {
         composable("pantallaFormulario1") { PantallaFormulario1(navHostController) }
         composable("pantalla2") { Pantalla2(navHostController) }
         composable("screenForm1") { ScreenForm1(navHostController) }
         composable("screenForm2") { ScreenForm2(navHostController) }
+        composable("loginScreen"){ LoginScreen(viewModel()) }
     }
 }
 
---
+++ app/src/main/java/com/appclass/myapplication/ui/login/ui/apuntes.md
@@ -0,0 +1,6 @@
+
+
+por cada vista, vamos a tener crear una **CLASE** que tenga sujeto el nombre de ViewModel
+    - por ejemplo de LoginScreen --> LoginViewModel
+
+ >pero LoginViewModel => es una clase JavaClass no un Kotlin-File
