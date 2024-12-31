815c6a3c71f8f000c4c29371345629737fd5f86c
beamm536 <beamm023@gmail.com>
2024-12-31 23:30:02 +0100

    no funciona la navegacion con mvvm :/

--- app/src/main/java/com/appclass/myapplication/MainActivity.kt
+++ app/src/main/java/com/appclass/myapplication/MainActivity.kt
@@ -7,22 +7,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.appclass.myapplication.navigation.NavigationWrapper
 import com.appclass.myapplication.ui.login.ui.LoginScreen
 import com.appclass.myapplication.ui.login.ui.LoginViewModel
-import com.appclass.myapplication.ui.theme.MyApplicationTheme
 
  class MainActivity : ComponentActivity() {
 
